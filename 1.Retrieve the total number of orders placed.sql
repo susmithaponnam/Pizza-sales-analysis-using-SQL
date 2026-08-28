317a2d28a4c25d8367151e68fbebe6eae6f165d7
Dakshayani Susmitha Ponnam <susmithaponnam21@gmail.com>
2026-08-28 23:49:19 +0530

    Add files via upload

---
+++ 1.Retrieve the total number of orders placed.sql
@@ -0,0 +1,4 @@
+-- Retrieve the total number of orders placed.
+
+select count(*) as total_orders
+from orders;
---
+++ 7.Determine the distribution of orders by hour of the day..sql
@@ -0,0 +1,7 @@
+-- Determine the distribution of orders by hour of the day.
+
+SELECT 
+    HOUR(order_time) AS Hour, COUNT(order_id) AS Order_count
+FROM
+    orders
+GROUP BY HOUR(order_time);
